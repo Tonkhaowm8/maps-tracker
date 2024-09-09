56d3652d6eee28ae3e925d08cb888a72bcff0ad3
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2024-09-09 15:52:31 +0900

    fix network ip

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Listen on all IP addresses (allow network access)
+    port: 3000        // Optional: specify the port (default is 3000)
+  },
 })
