d118b13c7044d1ddd653e479ef9bfd41e22f95ac
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2024-09-09 13:14:57 +0900

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MapComponent from './components/MapComponent';
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
